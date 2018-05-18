0c9e31890368a67f400ccd53e495dc35e260beb6
trevor.cooke101@gmail.com Cooke <trevor.cooke101@gmail.com>
2018-05-18 16:44:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, firstName){
 }
 
 function holidayCountdown(days, holiday){
-  return`It's ${days} "days until" ${holiday}!`
+  return`It's ${holiday} "days until" ${day}!`
 }
